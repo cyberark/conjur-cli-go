89d57178dbfea02024d4a16dbefb89e882983042
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2022-10-09 17:21:37 +0200

    Refactor variable command + add tests

--- pkg/cmd/variable.go
+++ pkg/cmd/variable.go
@@ -4,85 +4,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var variableCmd = &cobra.Command{
-	Use:   "variable",
-	Short: "A brief description of your command",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Print --help if called without subcommand
-		cmd.Help()
-	},
-}
+func NewVariableCmd(
+	getClientFactory variableGetClientFactoryFunc,
+	setClientFactory variableSetClientFactoryFunc,
+) *cobra.Command {
+	variableCmd := &cobra.Command{
+		Use:   "variable",
+		Short: "Use the variable command to manage Conjur variables",
+	}
 
-var variableGetCmd = &cobra.Command{
-	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	SilenceUsage: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		id, err := cmd.Flags().GetString("id")
-		if err != nil {
-			return err
-		}
-
-		conjurClient, err := authenticatedConjurClientForCommand(cmd)
-		if err != nil {
-			return err
-		}
-
-		// TODO: update RetrieveSecret to accept version
-		data, err := conjurClient.RetrieveSecret(id)
-		if err != nil {
-			return err
-		}
-
-		cmd.Println(string(data))
-		return nil
-	},
-}
-
-var variableSetCmd = &cobra.Command{
-	Use:   "set",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	SilenceUsage: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		id, err := cmd.Flags().GetString("id")
-		if err != nil {
-			return err
-		}
-
-		value, err := cmd.Flags().GetString("value")
-		if err != nil {
-			return err
-		}
-
-		conjurClient, err := authenticatedConjurClientForCommand(cmd)
-		if err != nil {
-			return err
-		}
-
-		err = conjurClient.AddSecret(id, value)
-		if err != nil {
-			return err
-		}
-
-		cmd.Println("Value added")
-		return nil
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(variableCmd)
+	variableGetCmd := NewVariableGetCmd(getClientFactory)
+	variableSetCmd := NewVariableSetCmd(setClientFactory)
 
 	variableCmd.AddCommand(variableGetCmd)
 	variableCmd.AddCommand(variableSetCmd)
@@ -99,6 +31,91 @@ func init() {
 	variableSetCmd.MarkFlagRequired("id")
 	variableSetCmd.Flags().StringP("value", "v", "", "Set the value of the specified variable")
 	variableSetCmd.MarkFlagRequired("value")
+
+	return variableCmd
+}
+
+type variableGetClient interface {
+	RetrieveSecret(string) ([]byte, error)
+}
+type variableSetClient interface {
+	AddSecret(string, string) error
+}
+
+type variableGetClientFactoryFunc func(*cobra.Command) (variableGetClient, error)
+type variableSetClientFactoryFunc func(*cobra.Command) (variableSetClient, error)
+
+func variableGetClientFactory(cmd *cobra.Command) (variableGetClient, error) {
+	return authenticatedConjurClientForCommand(cmd)
+}
+
+func variableSetClientFactory(cmd *cobra.Command) (variableSetClient, error) {
+	return authenticatedConjurClientForCommand(cmd)
+}
+
+func NewVariableGetCmd(clientFactory variableGetClientFactoryFunc) *cobra.Command {
+	return &cobra.Command{
+		Use:          "get",
+		Short:        "Use the get subcommand to get the value of one or more Conjur variables",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			id, err := cmd.Flags().GetString("id")
+			if err != nil {
+				return err
+			}
+
+			client, err := clientFactory(cmd)
+			if err != nil {
+				return err
+			}
+
+			// TODO: update RetrieveSecret to accept version
+			data, err := client.RetrieveSecret(id)
+			if err != nil {
+				return err
+			}
+
+			cmd.Println(string(data))
+			return nil
+		},
+	}
+}
+
+func NewVariableSetCmd(clientFactory variableSetClientFactoryFunc) *cobra.Command {
+	return &cobra.Command{
+		Use:          "set",
+		Short:        "Use the set subcommand to set the value of a Conjur variable",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			id, err := cmd.Flags().GetString("id")
+			if err != nil {
+				return err
+			}
+
+			value, err := cmd.Flags().GetString("value")
+			if err != nil {
+				return err
+			}
+
+			client, err := clientFactory(cmd)
+			if err != nil {
+				return err
+			}
+
+			err = client.AddSecret(id, value)
+			if err != nil {
+				return err
+			}
+
+			cmd.Println("Value added")
+			return nil
+		},
+	}
+}
+
+func init() {
+	variableCmd := NewVariableCmd(variableGetClientFactory, variableSetClientFactory)
+	rootCmd.AddCommand(variableCmd)
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
---
+++ pkg/cmd/variable_test.go
@@ -0,0 +1,114 @@
+package cmd
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cyberark/conjur-cli-go/pkg/testutils"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockVariableClient struct {
+	t   *testing.T
+	get func(*testing.T, string) ([]byte, error)
+	set func(*testing.T, string, string) error
+}
+
+func (m mockVariableClient) RetrieveSecret(path string) ([]byte, error) {
+	return m.get(m.t, path)
+}
+func (m mockVariableClient) AddSecret(path string, value string) error {
+	return m.set(m.t, path, value)
+}
+
+func TestVariableCmd(t *testing.T) {
+	tt := []struct {
+		name               string
+		args               []string
+		get                func(t *testing.T, path string) ([]byte, error)
+		set                func(t *testing.T, path string, value string) error
+		clientFactoryError error
+		out                string
+	}{
+		{
+			name: "variable command help",
+			args: []string{"--help"},
+			out:  "Use the variable command to manage Conjur variables",
+		},
+		{
+			name: "get subcommand help",
+			args: []string{"get", "--help"},
+			out:  "Use the get subcommand to get the value of one or more Conjur variables",
+		},
+		{
+			name: "set subcommand help",
+			args: []string{"set", "--help"},
+			out:  "Use the set subcommand to set the value of a Conjur variable",
+		},
+		{
+			name: "get subcommand",
+			args: []string{"get", "-i", "meow"},
+			get: func(t *testing.T, path string) ([]byte, error) {
+				// Assert on arguments
+				assert.Equal(t, "meow", path)
+
+				return []byte("moo"), nil
+			},
+			out: "moo",
+		},
+		{
+			name: "get subcommand missing required flag",
+			args: []string{"get"},
+			out:  "Error: required flag(s) \"id\" not set\n",
+		},
+		{
+			name:               "get client factory error",
+			args:               []string{"get", "-i", "moo"},
+			clientFactoryError: fmt.Errorf("%s", "client factory error"),
+			out:                "Error: client factory error\n",
+		},
+		{
+			name: "set subcommand",
+			args: []string{"set", "-i", "meow", "-v", "moo"},
+			set: func(t *testing.T, path, value string) error {
+				// Assert on arguments
+				assert.Equal(t, "meow", path)
+				assert.Equal(t, "moo", value)
+
+				return nil
+			},
+			out: "Value added",
+		},
+		{
+			name: "set subcommand missing required flag",
+			args: []string{"set"},
+			out:  "Error: required flag(s) \"id\", \"value\" not set\n",
+		},
+		{
+			name:               "set client factory error",
+			args:               []string{"set", "-i", "meow", "-v", "moo"},
+			clientFactoryError: fmt.Errorf("%s", "client factory error"),
+			out:                "Error: client factory error\n",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			mockClient := mockVariableClient{t: t, set: tc.set, get: tc.get}
+
+			cmd := NewVariableCmd(
+				func(cmd *cobra.Command) (variableGetClient, error) {
+					return mockClient, tc.clientFactoryError
+				},
+				func(cmd *cobra.Command) (variableSetClient, error) {
+					return mockClient, tc.clientFactoryError
+				},
+			)
+
+			out, _ := testutils.Execute(t, cmd, tc.args...)
+			assert.Contains(t, out, tc.out)
+		})
+	}
+}
