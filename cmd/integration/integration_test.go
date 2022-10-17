89fbfaa83bee2361f9c235d38f04f1b5aa82d9b4
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2022-10-17 12:45:14 +0100

    Refactor integration tests

--- cmd/integration/integration_test.go
+++ cmd/integration/integration_test.go
@@ -5,70 +5,12 @@ package main
 
 import (
 	"bytes"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
-	"os"
 	"os/exec"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-const pathToBinary = "conjur"
-
-func runCLIWithStdin(stdIn io.Reader, home string, args ...string) (stdOut string, stdErr string, err error) {
-	cmd := exec.Command(pathToBinary, args...)
-	stdOutBuffer := new(bytes.Buffer)
-	stdErrBuffer := new(bytes.Buffer)
-	cmd.Stdin = stdIn
-	cmd.Stdout = io.MultiWriter(stdOutBuffer, os.Stdout)
-	cmd.Stderr = io.MultiWriter(stdErrBuffer, os.Stderr)
-
-	cmd.Env = append(cmd.Env, "HOME="+home)
-
-	err = cmd.Run()
-	return stdOutBuffer.String(), stdErrBuffer.String(), err
-}
-
-func runCLI(home string, args ...string) (stdOut string, stdErr string, err error) {
-	return runCLIWithStdin(nil, home, args...)
-}
-
-func makeDevRequest(action string, params map[string]string) string {
-	url, _ := url.Parse("http://conjur/dev")
-	query := url.Query()
-	for k, v := range params {
-		query.Add(k, v)
-	}
-	query.Add("action", action)
-
-	url.RawQuery = query.Encode()
-
-	resp, err := http.Get(url.String())
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	return string(body)
-}
-
-func prepareConjurAccount(account string) func() {
-	makeDevRequest("purge", nil)
-	makeDevRequest("destroy_account", map[string]string{"id": account})
-	makeDevRequest("create_account", map[string]string{"id": account})
-	return func() {
-		makeDevRequest("destroy_account", map[string]string{"id": account})
-	}
-}
 
 func TestIntegration(t *testing.T) {
 	var (
@@ -78,41 +20,41 @@ func TestIntegration(t *testing.T) {
 	)
 	tmpDir := t.TempDir()
 	// Lean on the uniqueness of temp directories
-	id := strings.Replace(tmpDir, "/", "", -1)
-	account := id
+	account := strings.Replace(tmpDir, "/", "", -1)
+
+	conjurCLI := newConjurCLI(tmpDir)
 
 	t.Run("ensure binary exists", func(t *testing.T) {
 		_, err = exec.LookPath(pathToBinary)
 		assert.NoError(t, err)
 	})
 
-	// Setup brand new account in Conjur
 	cleanUpConjurAccount := prepareConjurAccount(account)
 	defer cleanUpConjurAccount()
 
 	t.Run("whoami before init", func(t *testing.T) {
-		stdOut, stdErr, err = runCLI(tmpDir, "whoami")
+		stdOut, stdErr, err = conjurCLI.Run("whoami")
 		assert.Error(t, err)
 		assert.Equal(t, "", stdOut)
 		assert.Equal(t, "Error: Missing required configuration for Conjur API URL\n", stdErr)
 	})
 
 	t.Run("init", func(t *testing.T) {
-		stdOut, stdErr, err = runCLI(tmpDir, "init", "-a", account, "-u", "http://conjur", "--force")
+		stdOut, stdErr, err = conjurCLI.Run("init", "-a", account, "-u", "http://conjur", "--force")
 		assert.NoError(t, err)
 		assert.Equal(t, "Wrote configuration to "+tmpDir+"/.conjurrc\n", stdOut)
 		assert.Equal(t, "", stdErr)
 	})
 
 	t.Run("login", func(t *testing.T) {
-		stdOut, stdErr, err = runCLI(tmpDir, "login", "-u", "admin", "-p", makeDevRequest("retrieve_api_key", map[string]string{"role_id": account + ":user:admin"}))
+		stdOut, stdErr, err = conjurCLI.Run("login", "-u", "admin", "-p", makeDevRequest("retrieve_api_key", map[string]string{"role_id": account + ":user:admin"}))
 		assert.NoError(t, err)
 		assert.Equal(t, "Logged in\n", stdOut)
 		assert.Equal(t, "", stdErr)
 	})
 
 	t.Run("whoami after init", func(t *testing.T) {
-		stdOut, stdErr, err = runCLI(tmpDir, "whoami")
+		stdOut, stdErr, err = conjurCLI.Run("whoami")
 		assert.NoError(t, err)
 		assert.Contains(t, stdOut, "token_issued_at")
 		assert.Contains(t, stdOut, "client_ip")
@@ -123,16 +65,15 @@ func TestIntegration(t *testing.T) {
 	})
 
 	t.Run("get variable before policy load", func(t *testing.T) {
-		stdOut, stdErr, err = runCLI(tmpDir, "variable", "get", "-i", "meow")
+		stdOut, stdErr, err = conjurCLI.Run("variable", "get", "-i", "meow")
 		assert.Error(t, err)
 		assert.Equal(t, "", stdOut)
 		assert.Contains(t, stdErr, "Error: 404 Not Found.")
 	})
 
 	t.Run("policy load", func(t *testing.T) {
-		stdOut, stdErr, err = runCLIWithStdin(
+		stdOut, stdErr, err = conjurCLI.RunWithStdin(
 			bytes.NewReader([]byte("- !variable meow")),
-			tmpDir,
 			"policy", "load", "-b", "root", "-f", "-",
 		)
 		assert.NoError(t, err)
@@ -142,14 +83,14 @@ func TestIntegration(t *testing.T) {
 	})
 
 	t.Run("set variable after policy load", func(t *testing.T) {
-		stdOut, stdErr, err = runCLI(tmpDir, "variable", "set", "-i", "meow", "-v", "moo")
+		stdOut, stdErr, err = conjurCLI.Run("variable", "set", "-i", "meow", "-v", "moo")
 		assert.NoError(t, err)
 		assert.Equal(t, "Value added\n", stdOut)
 		assert.Equal(t, "", stdErr)
 	})
 
 	t.Run("get variable after policy load", func(t *testing.T) {
-		stdOut, stdErr, err = runCLI(tmpDir, "variable", "get", "-i", "meow")
+		stdOut, stdErr, err = conjurCLI.Run("variable", "get", "-i", "meow")
 		assert.NoError(t, err)
 		assert.Equal(t, "moo\n", stdOut)
 		assert.Equal(t, "", stdErr)
