195a52cdbd602e509a27af98b414205c5f631c5f
Shlomo Heigh <shlomo.heigh@cyberark.com>
2022-12-27 12:49:21 -0500

    Set command description for conjur --help

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -9,13 +9,8 @@ import (
 func newRootCommand() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:   "conjur",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-	examples and usage of using your application. For example:
-	
-	Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`,
+		Short: "Conjur CLI",
+		Long:  "Command-line toolkit for managing Conjur resources and performing common tasks.",
 	}
 
 	rootCmd.PersistentFlags().Bool("verbose", false, "Verbose logging enabled")
