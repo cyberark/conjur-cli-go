55feb115e5f24b53f04daa781e61899d2f962f68
Shlomo Zalman Heigh <shlomo.heigh@cyberark.com>
2025-01-28 11:25:33 -0500

    CNJR-7752: Add integration test for variables with colons

--- cmd/integration/variable_integration_test.go
+++ cmd/integration/variable_integration_test.go
@@ -54,4 +54,24 @@ func TestVariableIntegration(t *testing.T) {
 		stdOut, stdErr, err := cli.Run("variable", "get", "-i", "meow", "-v", "3")
 		assertNotFound(t, err, stdOut, stdErr)
 	})
+
+	t.Run("variables with colons", func(t *testing.T) {
+		variablePolicy := `
+- !variable foo:bar
+- !variable foo:bar:baz
+`
+		cli.LoadPolicy(t, variablePolicy)
+
+		stdOut, stdErr, err := cli.Run("variable", "set", "-i", "foo:bar", "-v", "secretValue")
+		assertSetVariableCmd(t, err, stdOut, stdErr)
+
+		stdOut, stdErr, err = cli.Run("variable", "get", "-i", "foo:bar")
+		assertGetVariableCmd(t, err, stdOut, stdErr, "secretValue")
+
+		stdOut, stdErr, err = cli.Run("variable", "set", "-i", "foo:bar:baz", "-v", "otherSecretValue")
+		assertSetVariableCmd(t, err, stdOut, stdErr)
+
+		stdOut, stdErr, err = cli.Run("variable", "get", "-i", "foo:bar:baz")
+		assertGetVariableCmd(t, err, stdOut, stdErr, "otherSecretValue")
+	})
 }
